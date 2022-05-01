fd343a9f58c94bb6123a7855a3e6157fd52e1d05
Shyam Choudhary <shyamch728@gmail.com>
2022-05-02 00:56:58 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// let navbtn = document.getElementById('navbtn')
+// let navbar = document.querySelector('.navbar')
+// navbtn.addEventListener('click',showNav)
+
+// function showNav() {
+//     navbar.classList.toggle('showNav')
+// }
